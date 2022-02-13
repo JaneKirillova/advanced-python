7862c4400defdf8743ddabd0ed98de4a2e82c385
Jane Kirillova <grindevald666@gmail.com>
2022-02-14 00:38:06 +0300

    Add vizualizing function

--- hw_1/main.py
+++ hw_1/main.py
@@ -4,9 +4,13 @@ import networkx
 import fibonacci
 from visitor import ASTVisitor
 
-if __name__ == '__main__':
+
+def vizualize_ast(result_file):
     ast_object = ast.parse(inspect.getsource(fibonacci))
     visitor = ASTVisitor()
     visitor.visit(ast_object)
-    networkx.drawing.nx_pydot.to_pydot(visitor.graph).write_png("artifacts/ast.png")
+    networkx.drawing.nx_pydot.to_pydot(visitor.graph).write_png(result_file)
 
+
+if __name__ == '__main__':
+    vizualize_ast("artifacts/ast.png")
