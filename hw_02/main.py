d28e4e9459d076a054ae9fd0d158096fd967066b
Jane Kirillova <grindevald666@gmail.com>
2022-02-14 02:33:14 +0300

    Add medium task and requirements.txt

--- hw_02/main.py
+++ hw_02/main.py
@@ -1,9 +1,10 @@
 import os
 from functools import reduce
+from thebestastlibrary.main import vizualize_ast
 
 
 def generate_begin(title, author):
-    return "\\documentclass{article}\n\\usepackage[utf8]{inputenc}\n" + \
+    return "\\documentclass{article}\n\\usepackage{graphicx}\\usepackage[utf8]{inputenc}\n" + \
            f"\\title{{{title}}}\n\\author{{{author}}}\n" + \
            "\\begin{document}\n\\maketitle\n\\begin{center}"
 
@@ -28,25 +29,27 @@ def generate_table(table):
     ])
 
 
-def generate_full_file_content(title, author, table):
+def generate_image(image_location):
+    vizualize_ast(image_location)
+    return f"\\includegraphics[width=\\textwidth]{{{image_location}}}\n"
+
+
+def generate_full_file_content(title, author, table, image_location):
     return ''.join([
         generate_begin(title, author),
         generate_table(table),
+        generate_image(image_location),
         generate_end()
     ])
 
 
-def make_file(title, author, table):
+def make_file(title, author, table, image_location):
     with open("artifacts/table.tex", "w") as file:
-        file.write(generate_full_file_content(title, author, table))
+        file.write(generate_full_file_content(title, author, table, image_location))
     os.system(f"pdflatex -output-directory=artifacts artifacts/table.tex")
     os.system(f"rm artifacts/table.aux artifacts/table.log")
 
 
 if __name__ == '__main__':
-    # l = ["aaa", "bbbb", "cccc"]
-    # l2 = reduce(lambda lines, new_line: lines + "\\\\\n" + new_line, l)
-    # print(l2)
-    l3 = [["1", "2", "3"], ["13", "14", "14"], ["15", "16", "17"]]
-    make_file("aaa", "jane", l3)
-    # print(generate_full_file_content("aaa", "jane", l3))
+    table = [["1", "2", "3", "4"], ["hello", "from", "python", "script"], ["1111111", "longlonglonglong", "12", "34" ]]
+    make_file("Python task", "Evgeniia Kirillova", table, "artifacts/ast.png")
