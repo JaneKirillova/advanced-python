27b3d9f9e7382672d506f3fcbe52a21342a64fa4
JaneKirillova <grindevald666@gmail.com>
2022-03-16 19:30:30 +0300

    Add easy task

---
+++ hw_05/README.md
@@ -0,0 +1,13 @@
+## Run easy task:
+
+`python3 easy.py --num=<images number> --dir=<folder for images>`
+
+Default values:
+
+* `image number = 10`
+* `folder for image = 'artifacts'`
+
+
+
+Images are loaded from [https://picsum.photos/](https://picsum.photos/)
+
---
+++ hw_05/easy.py
@@ -0,0 +1,50 @@
+import asyncio
+import argparse
+import os
+
+import aiohttp
+import aiofiles
+
+
+async def download_site(url, session, file_name):
+    async with session.get(url) as response:
+        if response.status == 200:
+            async with aiofiles.open(file_name, mode="wb") as file:
+                await file.write(await response.read())
+
+
+async def download_all_sites(sites, path_to_files):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for image_number, url in enumerate(sites):
+            task = asyncio.create_task(download_site(url, session, f"{path_to_files}/image_{image_number}.jpg"))
+            tasks.append(task)
+        try:
+            await asyncio.gather(*tasks, return_exceptions=True)
+        except Exception as ex:
+            print(repr(ex))
+
+
+def make_arguments_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num', type=int, default=10)
+    parser.add_argument('--dir', type=str, default="artifacts")
+    return parser
+
+
+if __name__ == '__main__':
+    print(aiofiles.__version__)
+    # parser = make_arguments_parser()
+    # args = parser.parse_args()
+    #
+    # if not os.path.exists(args.dir):
+    #     os.makedirs(args.dir)
+    #
+    # sites = ["https://picsum.photos/200/300"] * args.num
+    #
+    # loop = asyncio.get_event_loop()
+    # try:
+    #     task = loop.create_task(download_all_sites(sites, args.dir))
+    #     loop.run_until_complete(task)
+    # finally:
+    #     loop.close()
